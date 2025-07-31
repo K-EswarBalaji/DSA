5005111dce9f01e7d6e8c34b60677b33b8954afb
K-EswarBalaji <kareeswarbalaji@gmail.com>
2025-07-31 10:20:13 +0530

    Update Array(multiolication).c

--- Array(multiolication).c
+++ Array(multiolication).c
@@ -7,7 +7,7 @@ int main(){
     int sum=0;
     for (int i=0;i<2;i++){
         for (int j=0;j<2;j++){
-                printf("Enter the value in %d %d poistion : ",i,j);
+                printf("Enter the value in %d %d position : ",i,j);
                 scanf("%d",&a[i][j]);
 
         }
@@ -15,10 +15,10 @@ int main(){
 
     for (int i=0;i<2;i++){
         for (int j=0;j<2;j++){
-                printf("\nThe value in %d %d poistion : %d",i,j,a[i][j]);
+                printf("\nThe value in %d %d position : %d",i,j,a[i][j]);
         }
     }
-
+    printf("\n");
     for (int i=0;i<2;i++){
         for (int j=0;j<2;j++){
             for(int k=0;k<2;k++){
@@ -29,14 +29,14 @@ int main(){
         }
     }
 
+    printf("\nThe Matrix multiplication is : \n");
+
     for (int i=0;i<2;i++){
         for (int j=0;j<2;j++){
-                printf("\n%d",result[i][j]);
+                printf("%d\t",result[i][j]);
         }
-
+        printf("\n");
     }
-
-
     return 0;
 }
 
